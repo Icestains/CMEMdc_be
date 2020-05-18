734b10ad970c9c5aac1aa7b0851a4546b8ab89b6
icestains <ourlichi@gmail.com>
2020-05-18 17:02:06 +0800

    抽离 file 方法

--- utils/logging/file.go
+++ utils/logging/file.go
@@ -1,8 +1,8 @@
 package logging
 
 import (
+	"CMEMdc_be/utils/file"
 	"fmt"
-	"log"
 	"os"
 	"time"
 
@@ -13,7 +13,7 @@ func getLogFilePath() string {
 	return fmt.Sprintf("%s%s", setting.AppSetting.RuntimeRootPath, setting.AppSetting.LogSavePath)
 }
 
-func getLogFileFullPath() string {
+func getLogFileName() string {
 	return fmt.Sprintf("%s%s.%s",
 		setting.AppSetting.LogSaveName,
 		time.Now().Format(setting.AppSetting.TimeFormat),
@@ -21,27 +21,28 @@ func getLogFileFullPath() string {
 	)
 }
 
-func openLogFile(filePath string) *os.File {
-	_, err := os.Stat(filePath)
-	switch {
-	case os.IsNotExist(err):
-		mkDir()
-	case os.IsPermission(err):
-		log.Fatalf("Permission :%v", err)
-	}
-
-	handle, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+func openLogFile(fileName, filePath string) (*os.File, error) {
+	dir, err := os.Getwd()
 	if err != nil {
-		log.Fatalf("Fail to OpenFile :%v", err)
+		return nil, fmt.Errorf("os.Getwd err: %v", err)
 	}
 
-	return handle
+	src := dir + "/" + filePath
+	perm := file.CheckPermission(src)
+	if perm == true {
+		return nil, fmt.Errorf("file.CheckPermission Permission denied src: %s", src)
+	}
+
+	err = file.IsNotExistMkDir(src)
+	if err != nil {
+		return nil, fmt.Errorf("file.IsNotExistMkDir src: %s, err: %v", src, err)
+	}
+
+	f, err := file.Open(src + fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("Fail to OpenFile :%v", err)
+	}
+
+	return f, nil
 }
 
-func mkDir() {
-	dir, _ := os.Getwd()
-	err := os.MkdirAll(dir+"/"+getLogFilePath(), os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-}
